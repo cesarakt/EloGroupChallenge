1858fa219579a9902dac0a454c41e32d004ea27b
Cesar Mazzaro <cesar_mazzaro1@hotmail.com>
2021-11-03 13:12:05 -0300

    Edição dos estilos com rbx e bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './views/Pages/Routes';
 
+import './styles/custom-bulma.scss';
+
 function App() {
   return (
     <Router>
